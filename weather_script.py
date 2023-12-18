95cff444d93c62014f3c0663e41780dda452570c
Paresh Rahool <62204249+paresh131@users.noreply.github.com>
2023-12-19 01:15:35 +0500

    Update weather_script.py

--- weather_script.py
+++ weather_script.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+# Assuming your Excel file is named 'cities.xlsx'
+cities_data = pd.read_excel('city_data_bi_developer_case_study_final (1).xlsx')
+
+# Print the data to check if it's being read correctly
+print(cities_data)
